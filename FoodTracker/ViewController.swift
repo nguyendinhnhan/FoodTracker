93fa3e726b0cbf50dfbe0e3a8a06988b16de5a87
Nhan Nguyen <nhannguyen.dad@gmail.com>
2019-07-16 17:58:00 +0700

    initial project

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Nguyen Dinh Nhan on 7/16/19.
+//  Copyright © 2019 Nguyen Dinh Nhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
